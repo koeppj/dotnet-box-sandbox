c9f74acf60814861d9a3a7a8957b1b45c61e9be9
John Koepp <jkoepp@box.com>
2025-05-20 17:17:31 -0400

    Initial Commit

---
+++ BoxApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Box.Sdk.Gen;
+using Box.Sdk.Gen.Schemas;
+
+var auth = new BoxJwtAuth(JwtConfig.FromConfigFile("../boxapp-config.json"));
+var client = new BoxClient(auth);
+
+var folder = await client.Folders.GetFolderItemsAsync("0");
+
+if (folder.Entries != null)
+{
+    Console.WriteLine("Items in the root folder:" + folder.Entries.Count);
+}
+else
+{
+    Console.WriteLine("No entries found in the root folder.");
+}
+
