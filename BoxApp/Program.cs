0a794f6e29438c730dc5abd1a1a48b3314809c1c
John Koepp <johnkoepp@gmail.com>
2025-05-20 21:08:01 -0400

    Nightly Updates

--- BoxApp/Program.cs
+++ BoxApp/Program.cs
@@ -1,18 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Box.Sdk.Gen;
-using Box.Sdk.Gen.Schemas;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
-var auth = new BoxJwtAuth(JwtConfig.FromConfigFile("../boxapp-config.json"));
-var client = new BoxClient(auth);
-
-var folder = await client.Folders.GetFolderItemsAsync("0");
-
-if (folder.Entries != null)
+public class BoxFolderLister
 {
-    Console.WriteLine("Items in the root folder:" + folder.Entries.Count);
+    private readonly BoxClient _client;
+
+    public BoxFolderLister(string configFile)
+    {
+        var auth = new BoxJwtAuth(JwtConfig.FromConfigFile(configFile));
+        _client = new BoxClient(auth);
+    }
+
+    public async Task ListFolderItemsAsync(string folderId)
+    {
+        var folder = await _client.Folders.GetFolderItemsAsync(folderId);
+
+        if (folder.Entries != null)
+        {
+            Console.WriteLine($"Items in folder {folderId}: {folder.Entries.Count}");
+        }
+        else
+        {
+            Console.WriteLine($"No entries found in folder {folderId}.");
+        }
+    }
 }
-else
+
+class Program
 {
-    Console.WriteLine("No entries found in the root folder.");
+    static async Task<int> Main(string[] args)
+    {
+        var configOption = new Option<string>(
+            "--config",
+            "Path to the Box JWT config JSON file"
+        )
+        { IsRequired = true };
+        var itemsCommand = new Command("items", "List items in the root folder");
+        var idOption = new Option<string>(
+            "--id",
+            "ID of the folder to list items from"
+        )
+        { IsRequired = true };
+        itemsCommand.AddOption(idOption);
+
+        itemsCommand.SetHandler(
+            async (string configFile, string id) =>
+            {
+                var lister = new BoxFolderLister(configFile);
+                await lister.ListFolderItemsAsync(id);
+            },
+            configOption, idOption
+        );
+
+        var rootCommand = new RootCommand("Box Folder Lister Tool");
+        rootCommand.AddOption(configOption);
+        rootCommand.AddCommand(itemsCommand);
+
+        return await rootCommand.InvokeAsync(args);
+    }
 }
 
